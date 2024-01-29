b7ce83c3a20919152a42e0dc5d239c2e3ac1784d
anto95240 <richard33antoine@gmail.com>
2024-01-29 18:58:08 +0100

    Orga + Navbar finish

---
+++ includes/nav.php
@@ -0,0 +1,34 @@
+<header>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="logo" href="#">
+        <img src="/assets/images/logo.png" class="img-fluid" alt="logo">
+      </a>  
+      <div class="input-group justify-content-center ms-auto">
+          <div class="col-auto">
+              <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="button-addon2">
+          </div>
+          <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+      </div>   
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="nav-tabs navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-light active text-uppercase" href="#">mon panier</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light text-uppercase" href="#">mon compte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light text-uppercase" href="#">mes commades</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light text-uppercase" href="#">mes factures</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once (__DIR__ . '/includes/header.php');
+// echo"Hello World";
+
+
+require_once (__DIR__ . '/includes/footer.php');
