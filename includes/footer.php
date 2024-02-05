f83539f44935225ad890a8347244f5567845fa3f
anto95240 <richard33antoine@gmail.com>
2024-02-05 16:29:05 +0100

    UPDATE btn nav + admin.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <script type="application/javascript" src="/assets/js/jquery-3.4.1.min.js"></script>
-<script type="application/javascript" src="/assets/js/bootstrap.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script type="application/javascript" src="/assets/js/jquery-ui.min.js"></script>
 <script type="application/javascript" src="/assets/js/script.js"></script>
 
--- src/admin/admin.php
+++ src/admin/admin.php
@@ -2,40 +2,55 @@
   require_once (__DIR__ . '/../../includes/header.php');
 ?>
 
-<main>
-
-        <div class="py-4">
+    <main>
+        <div class="py-5">
             <div class="container-fluid d-flex">
-                <div class="container create col-4">
+                <div class="container create col-3">
                     <div class="row d-flex flex-column">
-                        <h5>Création de produit</h5>
-                        <div class="form-column">
-                          <div class="form-group col-md-6">
-                            <label for="inputEmail4">Nom</label>
-                            <input type="email" class="form-control" id="inputEmail4" placeholder="Nom">
+                        <h5 class="pb-2">Création de produit</h5>
+                        <div class="form-column bg-light">
+                          <div class="form-group p-2">
+                            <label for="inputEmail4">Name</label>
+                            <textarea class="form-control" id="inputEmail4" placeholder="Name" rows="1"> </textarea>
                           </div>
-                          <div class="form-group col-md-6">
-                            <label for="inputEmail4">Prenom</label>
-                            <input type="email" class="form-control" id="inputEmail4" placeholder="Prenom">
+                          <div class="form-group p-2">
+                            <label for="inputEmail4">Description</label>
+                            <textarea class="form-control" id="inputEmail4" placeholder="Description" rows="3"> </textarea>
                           </div>
-                          <div class="form-group col-md-6">
-                            <label for="inputEmail4">Email</label>
-                            <input type="email" class="form-control" id="inputEmail4" placeholder="CoUrRiEl">
+                          <div class="form-group p-2">
+                            <label for="inputEmail4">Categorie</label>
+                            <textarea class="form-control" id="inputEmail4" placeholder="Catégorie" rows="1"> </textarea>
                           </div>
-                          <div class="form-group col-md-6">
-                            <label for="inputPassword4">Mot de Passe</label>
-                            <input type="password" class="form-control" id="inputPassword4" placeholder="Mot de Passe">
+                          <div class="form-group p-2">
+                            <label for="inputEmail4">Prix</label>
+                            <input type="email" class="form-control" id="inputEmail4" placeholder="Prix">
+                          </div>
+                          <div class="form-group p-2">
+                            <label for="inputPassword4">Quantité</label>
+                            <input type="password" class="form-control" id="inputPassword4" placeholder="Quantité">
                           </div>
                         </div>
+                        <div class="row pt-4">
+                            <div class="col-7 text-center">
+                                <a href="#" class="btn btn-primary">Ajouter</a>
+                            </div>
+                            <div class="col-0 text-center">
+                                <a href="#" class="btn btn-primary">Modifier</a>
+                            </div>
+                            <div class="col pt-3 text-center">
+                                <a href="#" class="btn btn-primary">Supprimer</a>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
-                <div class="container cat col-2 py-5">
+                <div class="container cat col-2 ">
                     <div class="row">
                         <h5>Liste des catégories</h5>
                     </div>
                 </div>
 
-                <div class="container product col-6 text-center py-5">
+                <div class="container product col-6 text-center">
                     <div class="row">
                         <div class="col-6 my-1">
                             <div class="card" style="width: 18rem;">
