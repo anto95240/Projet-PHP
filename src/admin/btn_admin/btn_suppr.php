4fb02841f02ab0a48edfd4267b9c04029b26f846
anto95240 <richard33antoine@gmail.com>
2024-02-14 13:59:00 +0100

    UPDATE btn_suppr.php

--- src/admin/btn_admin/btn_suppr.php
+++ src/admin/btn_admin/btn_suppr.php
@@ -12,14 +12,14 @@ if (isset($_POST['product_delete_multiple_btn'])) {
     $product_query_run = mysqli_query($con, $product_query);
 
     // Suppression des catégories associées aux produits supprimés
-    $category_query = "DELETE FROM categorie_table WHERE id IN (SELECT CategorieId FROM product_table WHERE ProductId IN($extract_id))";
+    $category_query = "DELETE FROM categorie_table WHERE CategorieId IN (SELECT CategorieId FROM product_table WHERE ProductId IN($extract_id))";
     $category_query_run = mysqli_query($con, $category_query);
 
     if ($product_query_run && $category_query_run) {
         $_SESSION['status'] = "Products deleted successfully";
-        header("Location: admin.php");
+        header("Location: /src/admin/admin.php");
     } else {
         $_SESSION['status'] = "Products not deleted";
-        header("Location: admin.php");
+        header("Location: /src/admin/admin.php");
     }
 }
