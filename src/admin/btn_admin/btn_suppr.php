4e9972dfc2ad32a44d4783e70ad2aa185571d7d8
anto95240 <richard33antoine@gmail.com>
2024-02-24 16:34:31 +0100

    UPDATE btn_suppr

--- src/admin/btn_admin/btn_suppr.php
+++ src/admin/btn_admin/btn_suppr.php
@@ -1,25 +1,35 @@
 <?php
-session_start();
 
-$con = mysqli_connect("localhost", "root", "", "e-commerce");
+require_once (__DIR__ . '/../../../config/database.php');
 
-if (isset($_POST['product_delete_multiple_btn'])) {
-    $all_id = $_POST['product_delete_id'];
-    $extract_id = implode(',', $all_id);
+// Ajout d'un produit a l'aide des checkbox
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['product_delete_multiple_btn'])) {
+    deleteProducts($_POST['product_delete_id']);
+}
 
-    // Suppression des produits
-    $product_query = "DELETE FROM product_table WHERE ProductId IN($extract_id)";
-    $product_query_run = mysqli_query($con, $product_query);
+function deleteProducts($productIds) {
+    global $access;
 
-    // Suppression des catégories associées aux produits supprimés
-    $category_query = "DELETE FROM categorie_table WHERE CategorieId IN (SELECT CategorieId FROM product_table WHERE ProductId IN($extract_id))";
-    $category_query_run = mysqli_query($con, $category_query);
+    // Converti les ProduitId en une chaîne séparée par des virgules
+    $productIdsString = implode(',', $productIds);
+    
+    // Supprime les catégories associées aux produits supprimés
+    $deleteCategoriesQuery = $access->prepare("DELETE FROM categorie_table WHERE CategorieId IN (SELECT CategorieId FROM product_table WHERE ProductId IN ($productIdsString))");
+    $deleteCategoriesQuery->execute();
 
-    if ($product_query_run && $category_query_run) {
+    // Supprime les produits de la table product_table
+    $deleteProductsQuery = $access->prepare("DELETE FROM product_table WHERE ProductId IN ($productIdsString)");
+    $deleteProductsQuery->execute();
+
+
+    // Vérifier si les suppressions ont réussi
+    if ($deleteProductsQuery && $deleteCategoriesQuery) {
         $_SESSION['status'] = "Products deleted successfully";
-        header("Location: /src/admin/admin.php");
     } else {
-        $_SESSION['status'] = "Products not deleted";
-        header("Location: /src/admin/admin.php");
+        $_SESSION['status'] = "Error: Products not deleted";
     }
+    
+    // Redirige vers la page admin.php après que la suppression ait réussit
+    header("Location: /src/admin/admin.php");
+    exit();
 }
