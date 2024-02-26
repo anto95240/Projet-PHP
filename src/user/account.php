cc09daeeb71fda718508227722d325745233552c
anto95240 <richard33antoine@gmail.com>
2024-02-26 09:57:03 +0100

    UPDATE account.php

--- src/user/account.php
+++ src/user/account.php
@@ -3,6 +3,7 @@
   $currentPage = 'moncompte';
   require_once (__DIR__ . '/../../includes/header.php');
   require_once (__DIR__ . '/../admin/affichage.php');
+  require_once (__DIR__ . '/../../config/database.php');
 
   $userId = $_SESSION['user_id']; // Supposons que vous stockez l'ID de l'utilisateur dans la session
   $query = "SELECT a.*, u.*
@@ -15,31 +16,71 @@
   $stmt->execute();
   $user = $stmt->fetch(PDO::FETCH_ASSOC); // Récupérer les résultats de la requête
     
+
+// Mise à jour d'un user
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_user'])) {
+  // Récupère les valeurs des champs de formulaire
+  $userId = $_POST['user_userId'];
+  $addressId = $_POST['address_addressId'];
+  $firstName = $_POST['address_firstName'];
+  $lastName = $_POST['address_lastName'];
+  $email = $_POST['user_email'];
+  $password = $_POST['user_password'];
+  $address = $_POST['address_streetAddress'];
+  $city = $_POST['address_city'];
+  $state = $_POST['address_state'];
+  $country = $_POST['address_country'];
+  $postalCode = $_POST['address_postalCode'];
+
+  updateUser($userId, $addressId, $firstName, $lastName, $email, $password, $address, $city, $state, $country, $postalCode);
+}
+
+function updateUser($userId, $addressId, $firstName, $lastName, $email, $password, $address, $city, $state, $country, $postalCode) {
+  global $access;
+
+  // Mise à jour du user dans la table user_table
+
+  $updateUserQuery = $access->prepare("UPDATE user_table SET Email = ?, Password = ? WHERE UserId = ?");
+  $updateUserQuery->execute([$email, $password, $userId]); 
+
+  // Mise à jour du user dans la table address_table
+  $updateAddressQuery = $access->prepare("UPDATE address_table SET UserId = ?, FirstName = ?, LastName = ?, StreetAddress = ?, City = ?, State = ?, Country = ?, PostalCode = ? WHERE AdressId = ?");
+  $updateAddressQuery->execute([$userId, $firstName, $lastName, $address, $city, $state, $country, $postalCode, $addressId]);
+
+  // Vérifier si les mises à jour ont réussi
+  if ($updateAddressQuery && $updateAddressQuery) { 
+      $_SESSION['status'] = "Products updated successfully";
+  } else {
+      $_SESSION['status'] = "Error: Products not updated";
+  }
+
+}
+
 ?>
 <section class="ps-3" >
   <h5 class="mt-4"> Mon Compte </h5>
   <!-- on cree ensuite la structure de la page web ! (ici un formulaire)-->
   <div class="container mt-3 mx-auto mb-5 bg-success rounded p-2" style="background-color: mediumseagreen !important;">
   <!-- Bootstrap permet de simplifier l'utilisation de css par le biais de raccourcis (mt pour margin top 5px/ succes est leur couleur verte, danger rouge...) -->
-    <form method="post" action="update_account.php" class="d-flex flex-column gap-3 ps-5">
+    <form method="post" class="d-flex flex-column gap-3 ps-5">
       <div class="row d-flex justify-content-beetwen">
         <div class="col-md-6">
           <div class="d-flex gap-5 flex-column">
             <div class="form-group col-md-6">
               <label>Prenom</label>
-              <input type="text" class="form-control" id="address_lastName" name="address_lastName" placeholder="Prenom" value="<?php echo $user['FirstName']; ?>">
+              <input type="text" class="form-control" name="address_lastName" placeholder="Prenom" value="<?php echo $user['FirstName']; ?>">
             </div>
             <div class="form-group col-md-6">
               <label>Nom</label>
-              <input type="text" class="form-control"  id="address_firstName" name="address_firstName" placeholder="Nom" value="<?php echo $user['LastName']; ?>">
+              <input type="text" class="form-control" name="address_firstName" placeholder="Nom" value="<?php echo $user['LastName']; ?>">
             </div>
              <div class="form-group col-md-6">
               <label>Email</label>
-              <input type="email" class="form-control" id="user_email" name="user_email" placeholder="CoUrRiEl" value="<?php echo $user['Email']; ?>">
+              <input type="email" class="form-control" name="user_email" placeholder="CoUrRiEl" value="<?php echo $user['Email']; ?>">
             </div>
             <div class="form-group col-md-6">
               <label>Mot de Passe</label>
-              <input type="password" class="form-control" id="user_password" name="user_password" placeholder="Mot de Passe" value="<?php echo $user['Password']; ?>">
+              <input type="password" class="form-control" name="user_password" placeholder="Mot de Passe" value="<?php echo $user['Password']; ?>">
             </div>
           </div>
         </div>
@@ -48,19 +89,19 @@
           <div class="d-flex gap-5 flex-column align-items-end">
               <div class="form-group col-md-6">
                 <label>Adresse</label>
-                <input type="text" class="form-control" id="address_streetAddress" name="address_streetAddress" placeholder="1 rue de l'Imposture" value="<?php echo $user['StreetAddress']; ?>">
+                <input type="text" class="form-control" name="address_streetAddress" placeholder="1 rue de l'Imposture" value="<?php echo $user['StreetAddress']; ?>">
               </div>
               <div class="form-group col-md-6">
                 <label>Ville</label>
-                <input type="text" class="form-control" id="address_city" name="address_city"  value="<?php echo $user['City']; ?>">
+                <input type="text" class="form-control" name="address_city"  value="<?php echo $user['City']; ?>">
               </div>
               <div class="form-group col-md-6">
                 <label>Etat / Région</label>
-                <input type="text" class="form-control" id="address_state" name="address_state"  value="<?php echo $user['State']; ?>" placeholder="Etat / Région">
+                <input type="text" class="form-control" name="address_state"  value="<?php echo $user['State']; ?>" placeholder="Etat / Région">
               </div>
               <div class="form-group col-md-6">
                 <label>Pays</label>
-                <select id="address_country" name="address_country" class="form-control">
+                <select name="address_country" class="form-control">
                   <option selected><?php echo $user['Country']; ?></option>
                   <option>Afghanistan</option>
                   <option>Afrique du Sud</option>
@@ -261,12 +302,12 @@
               </div>
               <div class="form-group col-md-3">
                 <label>Code Postal</label>
-                <input type="text" class="form-control" id="address_postalCode" name="address_postalCode" value="<?php echo $user['PostalCode']; ?>">
+                <input type="text" class="form-control" name="address_postalCode" value="<?php echo $user['PostalCode']; ?>">
               </div>
           </div>
         </div>
       </div>
-      <button type="submit" class="btn btn-primary  w-25 flex-end">Modifier</button>
+      <button type="submit" name="update_user" class="btn btn-primary w-25 flex-end">Modifier</button>
       <input type="hidden" name="user_userId" value="<?= $user['UserId']; ?>">
       <input type="hidden" name="address_addressId" value="<?= $user['AddressId']; ?>">
     </form>
